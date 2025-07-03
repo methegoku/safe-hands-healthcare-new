31552c38c3f50ca1afd1ec21d556551c770bb81e
Gokul Elumalai <gokulelumalai46@gmail.com>
2025-07-03 18:25:06 +0530

    deleted server prt

--- server/controllers/categoryController.js
+++
@@ -1,6 +0,0 @@
-const { ServiceCategory } = require('../models');
-
-exports.getAll = async (req, res) => {
-  const categories = await ServiceCategory.findAll();
-  res.json(categories);
-};
--- server/routes/cities.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cityController = require('../controllers/cityController');
-
-router.get('/', cityController.getAll);
-router.get('/:id', cityController.getById);
-
-module.exports = router;
